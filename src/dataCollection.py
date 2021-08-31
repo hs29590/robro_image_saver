2e2a4fc12e6be7d5c5a508cadb736e61f073bc53
robro-ayush <76692383+robro-ayush@users.noreply.github.com>
2021-08-31 13:03:18 +0530

    added subscriber for category

--- src/dataCollection.py
+++ src/dataCollection.py
@@ -23,6 +23,8 @@ import rospy
 
 from sensor_msgs.msg import Image
 
+from std_msgs.msg import String
+
 rospy.init_node('dataCollection', anonymous=True)
 
 proj_name = rospy.get_param('~proj_name')
@@ -30,11 +32,18 @@ ros_topic_name = rospy.get_param('~ros_topic_name')
 image_type = rospy.get_param('~image_type')
 camera_name = rospy.get_param('~camera_name')
 cred_path = rospy.get_param('~cred_path')
+category = 'DEFAULT'
 try:
-    category_valid = True
+    category_param_valid = True
     category = rospy.get_param('~category')
 except:
-    category_valid = False
+    category_param_valid = False
+
+try:
+    category_topic_valid = True
+    category_topic_name = rospy.get_param('~category_topic_name')
+except:
+    category_topic_valid = False
 
 try:
     resize_q = True
@@ -68,34 +77,48 @@ uploader_obj = RobroBucketImageUploader(
     proj_name, image_type, proj_name, cred_path)
 
 
+def categoryRecievedCallback(data):
+    """Recieves Category of image.
+
+    Args:
+        data ([String])
+    """
+    print('Recieved new Category')
+    global category
+    category = data.data
+
+
 def imageRecievedCallback(data):
     """Recieves ROS image converts to CV Image, apply transformations and upload.
 
     Args:
         data ([ROS Image])
     """
+    print('Recieved Image')
     try:
         cv_image = CvBridge().imgmsg_to_cv2(data, 'passthrough')
     except CvBridgeError as e:
         print(e)
-    if resize_q:
-        dim = (int(image_size.split(',')[0]), int(image_size.split(',')[1]))
-        cv_image = cv2.resize(cv_image, dim, interpolation=cv2.INTER_AREA)
     if crop_q:
         cv_image = cv_image[
             int(crop_size.split(',')[1]):
             int(crop_size.split(',')[1])+int(crop_size.split(',')[3]),
             int(crop_size.split(',')[0]):int(crop_size.split(',')[0])
             + int(crop_size.split(',')[2])]
+    if resize_q:
+        dim = (int(image_size.split(',')[0]), int(image_size.split(',')[1]))
+        cv_image = cv2.resize(cv_image, dim, interpolation=cv2.INTER_AREA)
     if grayscale:
         cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-    if category_valid:
+    if category_param_valid or category_topic_valid:
         uploader_obj.addToUploadQueue(cv_image, camera_name, category)
     else:
         uploader_obj.addToUploadQueue(cv_image, camera_name)
 
 
 image_sub = rospy.Subscriber(ros_topic_name, Image, imageRecievedCallback)
+if category_topic_valid:
+    category_sub = rospy.Subscriber(
+        category_topic_name, String, categoryRecievedCallback)
 
 rospy.spin()
-
